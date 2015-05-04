e7e81410966a2f8c35debd941cf08d647940524c
gilang <gilang.julyana@mail.unpas.ac.id>
2015-05-04 12:47:19 +0700

    asdf

--- Ujian_123040109/src/main/java/com/mycompany/ujian_123040109/Soal.java
+++ Ujian_123040109/src/main/java/com/mycompany/ujian_123040109/Soal.java
@@ -31,6 +31,6 @@ public class Soal
         }
        public static void main( String[] args )
     {
-        System.out.println( "Hello World!2" );
+        System.out.println( "Hello World!3" );
     }
 }
