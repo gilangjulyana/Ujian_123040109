302ca0bb876b612ef9874e328afe69ae2379c452
gilang <gilang.julyana@mail.unpas.ac.id>
2015-05-04 12:30:07 +0700

    initial commit 1

---
+++ Ujian_123040109/src/main/java/com/mycompany/ujian_123040109/Soal.java
@@ -0,0 +1,33 @@
+package com.mycompany.ujian_123040109;
+
+/**
+ * Hello world!
+ *
+ */
+public class Soal 
+{
+    public int pangkat(int bil, int pangkat){
+        int hasil = 0;
+        String h = "hallo2";
+        int n = 1;
+        while (n <= pangkat) {            
+            n++;
+            hasil *= bil;
+        }
+        return hasil;
+        
+    }
+    public int faktorial(int bil){
+        int hasil = 1;
+        for (int i = 1; i <= bil; i++) {
+            hasil *= i;
+        }
+        return hasil;
+    }
+ 
+    public int perkalian(int bilA, int bilB){
+        return bilA * bilB;
+        
+        }
+   
+}
